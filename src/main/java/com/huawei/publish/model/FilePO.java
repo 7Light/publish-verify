ae1323189507736fa7a3a453a19e83eb2d26501b
xiongfengbo <xiongfengbo@huawei.com>
2022-05-18 11:24:07 +0800

    1

--- src/main/java/com/huawei/publish/model/FilePO.java
+++ src/main/java/com/huawei/publish/model/FilePO.java
@@ -1,14 +1,59 @@
 package com.huawei.publish.model;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class FilePO {
     private String name;
     private String size;
     private String Date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getParentDir() {
+        return parentDir;
+    }
+
+    public void setParentDir(String parentDir) {
+        this.parentDir = parentDir;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getSha256() {
+        return sha256;
+    }
+
+    public void setSha256(String sha256) {
+        this.sha256 = sha256;
+    }
+
     private String parentDir = "";
     private String url;
     private String sha256;
--- src/main/java/com/huawei/publish/model/PublishPO.java
+++ src/main/java/com/huawei/publish/model/PublishPO.java
@@ -1,13 +1,65 @@
 package com.huawei.publish.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.List;
 
-@Getter
-@Setter
+
 public class PublishPO {
+    public String getGpgKeyUrl() {
+        return gpgKeyUrl;
+    }
+
+    public void setGpgKeyUrl(String gpgKeyUrl) {
+        this.gpgKeyUrl = gpgKeyUrl;
+    }
+
+    public String getKeyFileName() {
+        return keyFileName;
+    }
+
+    public void setKeyFileName(String keyFileName) {
+        this.keyFileName = keyFileName;
+    }
+
+    public String getRpmKey() {
+        return rpmKey;
+    }
+
+    public void setRpmKey(String rpmKey) {
+        this.rpmKey = rpmKey;
+    }
+
+    public String getFileKey() {
+        return fileKey;
+    }
+
+    public void setFileKey(String fileKey) {
+        this.fileKey = fileKey;
+    }
+
+    public String getTempDir() {
+        return tempDir;
+    }
+
+    public void setTempDir(String tempDir) {
+        this.tempDir = tempDir;
+    }
+
+    public String getPublishDir() {
+        return publishDir;
+    }
+
+    public void setPublishDir(String publishDir) {
+        this.publishDir = publishDir;
+    }
+
+    public List<FilePO> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<FilePO> files) {
+        this.files = files;
+    }
+
     private String gpgKeyUrl;
     private String keyFileName;
     private String rpmKey;
